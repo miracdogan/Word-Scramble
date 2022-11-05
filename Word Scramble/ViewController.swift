83ed590ad2ff562e94f692fc968f32f73b248730
Miraç Doğan <miracdogn@gmail.com>
2022-11-05 22:27:08 +0300

    Initial Commit

---
+++ Word Scramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Word Scramble
+//
+//  Created by Miraç Doğan on 5.11.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
